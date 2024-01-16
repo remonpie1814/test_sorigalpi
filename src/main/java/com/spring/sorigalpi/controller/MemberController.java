b814cc973854c8c793ca498d38caf5f1c4bf94d6
RyuSeohyeong <ryugowell@gmail.com>
2024-01-17 00:02:56 +0900

    FEAT: 사용자 목록 조회 추 #1

--- src/main/java/com/spring/sorigalpi/controller/MemberController.java
+++ src/main/java/com/spring/sorigalpi/controller/MemberController.java
@@ -1,18 +1,17 @@
 package com.spring.sorigalpi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spring.sorigalpi.dto.LoginDto;
 import com.spring.sorigalpi.dto.SignUpDto;
 import com.spring.sorigalpi.entity.Member;
 import com.spring.sorigalpi.service.MemberService;
-import com.spring.sorigalpi.service.TestException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -50,6 +49,11 @@ public class MemberController {
 	@PostMapping("/signUp")
 	public String save(@RequestBody SignUpDto signUpDto) {
 		return memberService.save(signUpDto);
+	}
+	
+	@GetMapping("/members")
+	public List<Member> findAll(){
+		return (List<Member>) memberService.findAll();
 	}
 	
 	}
