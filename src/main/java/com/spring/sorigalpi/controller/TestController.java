022939703443e3fd9166e70b5bd186daf633dcea
choi09 <0930alan@gmail.com>
2024-01-23 15:52:13 +0900

    Swagger추가 http://localhost:8080/swagger-ui/index.html 로 접속 가능

--- src/main/java/com/spring/sorigalpi/controller/TestController.java
+++ src/main/java/com/spring/sorigalpi/controller/TestController.java
@@ -3,6 +3,9 @@ package com.spring.sorigalpi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
+@Api(tags = "test")
  @RestController
 public class TestController {
 
