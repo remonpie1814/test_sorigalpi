5038b7b681917666fbd7dfd124cfdf6a53c41477
RyuSeohyeong <ryugowell@gmail.com>
2024-01-30 01:12:32 +0900

    Refactor: Enum Type 패키 따로 생성 후 재조정

---
+++ src/main/java/com/spring/sorigalpi/enumtype/MemberEnum.java
@@ -0,0 +1,12 @@
+package com.spring.sorigalpi.enumtype;
+
+public class MemberEnum {
+	public enum Role{ // USER: 사용자, ADMIN: 관리
+		USER, ADMIN
+	}
+	
+	public enum Status{ //ACTIVE: 활동중, QUIT: 탈퇴, BLOCK: 차
+		ACTIVE, QUIT, BLOCK
+	}
+
+}
