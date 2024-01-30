d2b57118af227fda9aee8013f8cfbeb0a2e655de
choi09 <0930alan@gmail.com>
2024-01-30 20:43:51 +0900

    Feat: 동화책 Id로 동화책 정보 삭제 추가

--- src/main/java/com/spring/sorigalpi/service/BookService.java
+++ src/main/java/com/spring/sorigalpi/service/BookService.java
@@ -23,12 +23,28 @@ public class BookService extends Base{
 		return bookRepository.findAll();
 	}
 	
-	public String createBook(BookDTO bookDTO) {
+	public String createBook(BookDTO bookDTO) { //동화책 생성
 		bookDTO.setBookId(createRandomUuId());
 		
 		System.out.println(bookDTO.getBookId());
 		
 		return bookRepository.save(bookDTO.toEntity()).getBookId();
+	}
+	
+	public String deleteBookById(BookDTO bookDTO) { //동화책 ID로 삭제
+		
+		String result;
+		String bookId = bookDTO.getBookId();
+		
+		try {
+			bookRepository.deleteById(bookId);
+			result = "삭제 성공";
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = "삭제 실패 해당 동화책이 존재하지 않습니다.";
+		}
+		
+		return result;
 	}
 	
 }
