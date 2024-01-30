7da1787d8001d0af03bad11aafb3d166f9a832dd
RyuSeohyeong <ryugowell@gmail.com>
2024-01-30 15:05:27 +0900

    Comment:메소드 설명하는 주석 추가

--- src/main/java/com/spring/sorigalpi/service/MemberService.java
+++ src/main/java/com/spring/sorigalpi/service/MemberService.java
@@ -23,29 +23,29 @@ public class MemberService extends Base {
     private final MemberRepository memberRepository;
     
     @Transactional
-    public String createMember(MemberDto memberDto) { //사용자 추가 메소드
+    public String createMember(MemberDto memberDto) { // 사용자 추가 메소드
     	memberDto.setMemberId(createRandomUuId());;
     	return memberRepository.save(memberDto.toEntity()).getNickName() + "님 환영합니다.";
     }
     
-    public List<Member> listMembers(){
+    public List<Member> listMembers(){ // 사용자 조회 메소드
     	return memberRepository.findAll();
     }
     @Transactional
-    public String updateMember(String memberId, MemberDto memberDto) {
+    public String updateMember(String memberId, MemberDto memberDto) { // 사용자 정보 변경 메소
     	//findById 메소드를 통해 값을 가져오면서 해당 값은 영속성을 가진다.
     	Member member = memberRepository.findById(memberId).orElseThrow(
     			() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
     	//값 변경
     	member.updateMember(memberDto.getEmail(), memberDto.getPwd(), memberDto.getNickName(),
     			memberDto.getProfileImg(), memberDto.getStatus(),memberDto.getIntro(), memberDto.getRole());
-    	return memberId;
+    	return memberDto.getNickName() + "님 정보가 변경되었습니다.";
     	//트랜잭션이 끝나면서 변경된 값을 테이블에 적용
     	//update기능에서 JPA영속성 때문에 DB에 쿼리를 없애는 부분이 없으며, Entity의 값만 변경하면 별도로 update쿼리가 필요없다.
     }
     
     @Transactional
-    public String deleteMember (String memberId) {
+    public String deleteMember (String memberId) { // 사용자 삭제 메소드
     	Member member = memberRepository.findById(memberId).orElseThrow(() -> {
     		return new IllegalArgumentException("해당 사용자가 존재하지 않습니다.");
     	});
@@ -54,7 +54,7 @@ public class MemberService extends Base {
     	return "삭제 완료";
     }
     
-    public String login(MemberDto memberDto) {
+    public String login(MemberDto memberDto) { // 로그인 메소
         String email = memberDto.getEmail();
         String rawPassword = memberDto.getPwd();
 
